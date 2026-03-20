f66bed2508070c02a5c9ac914156e377f2c1252a
Simcaa007 <havelkova.si@seznam.cz>
2026-03-20 14:14:37 +0100

    hotovo

---
+++ 07/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                while (!sr.EndOfStream)
+                {
+                    string radek = sr.ReadLine();
+                    if ((radek[0] >= 'A' && radek[0] <= 'Z') && radek[radek.Length - 1] == '.')
+                    {
+                        pocet++;
+                    }
+                }
+            }
+            MessageBox.Show(pocet.ToString());
+        }
+    }
+}
